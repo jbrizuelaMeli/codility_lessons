d27eac208c47024d2fa843e36e5378812818c45c
jbrizuelaMeli <juan.brizuela@mercadolibre.com>
2021-09-26 18:11:32 -0300

    minor fix

--- arrays/cyclic_rotation.go
+++ arrays/cyclic_rotation.go
@@ -5,7 +5,7 @@ type CyclicRotation struct{}
 //Given an array A consisting of N integers and an integer K, returns the array A rotated K times.
 func (cr *CyclicRotation) Solution(a []int, k int) []int {
 	l := len(a)
-	if k == 0 || k%l == 0 {
+	if k == 0 || l == 0 || k%l == 0 {
 		return a
 	}
 	b := make([]int, l)
