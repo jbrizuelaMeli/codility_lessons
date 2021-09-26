14bfeacee0418b2fd480de73bfc94c2e5b31649d
jbrizuelaMeli <juan.brizuela@mercadolibre.com>
2021-09-26 14:40:25 -0300

    added first lesson iteration, count binary gaps

---
+++ iterations/iterations.go
@@ -0,0 +1,32 @@
+package iterations
+
+import (
+	"strconv"
+)
+
+//Given a positive integer N, returns the length of its longest binary gap.
+//The function should return 0 if N doesn't contain a binary gap.
+func Solution(n int) int {
+	//Discard even numbers or 1
+	if n == 1 {
+		return 0
+	}
+	//to binary form -> 137 = 10001001
+	nb := strconv.FormatInt(int64(n), 2)
+	//To array of bytes
+	nbb := []byte(nb)
+	var gap int
+	var gapAux int
+	for idx, val := range nbb {
+		if val == byte('1') && idx > 0 {
+			if gapAux > gap {
+				gap = gapAux
+			}
+			gapAux = 0
+		}
+		if val == byte('0') {
+			gapAux++
+		}
+	}
+	return gap
+}
---
+++ iterations/iterations_test.go
@@ -0,0 +1,35 @@
+package iterations
+
+import (
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func TestSolution(t *testing.T) {
+	n := 328
+
+	res := Solution(n)
+	assert.Equal(t, 2, res)
+}
+
+func TestSolution2(t *testing.T) {
+	n := 123443
+
+	res := Solution(n)
+	assert.Equal(t, 3, res)
+}
+
+func TestSolution3(t *testing.T) {
+	n := 345365628
+
+	res := Solution(n)
+	assert.Equal(t, 3, res)
+}
+
+func TestSolution4(t *testing.T) {
+	n := 3099887828
+
+	res := Solution(n)
+	assert.Equal(t, 3, res)
+}
