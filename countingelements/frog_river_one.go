30f5cf61bcbc98965c3ab863388bc90463b3dc03
jbrizuelaMeli <juan.brizuela@mercadolibre.com>
2021-09-28 23:16:53 -0300

    adding comments

--- countingelements/frog_river_one.go
+++ countingelements/frog_river_one.go
@@ -4,6 +4,8 @@ type FrogRiverOne struct{}
 
 // given a non-empty array a consisting of N integers and integer x,
 //returns the earliest time when the frog can jump to the other side of the river.
+//The frog can cross only when leaves appear at every position across the river from 1 to x (that is,
+//we want to find the earliest moment when all the positions from 1 to x are covered by leaves)
 //If the frog is never able to jump to the other side of the river, the function should return −1.
 func (f FrogRiverOne) Solution(x int, a []int) int {
 	l := len(a)
